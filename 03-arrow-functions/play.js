7e1929731aa572280cbf706954bf6f6e44d66634
saci zakaria <sacideveloppeur@gmail.com>
2024-01-29 15:09:07 +0100

    return a + b;

--- 03-arrow-functions/play.js
+++ 03-arrow-functions/play.js
@@ -14,21 +14,23 @@ const summarizeUser = (userName, userAge, userHasHobby) => {
     userHasHobby
   );
 };
-console.log(summarizeUser(name, age, hasHobbies));
+//console.log(summarizeUser(name, age, hasHobbies));
 
 
 
-
+const add = (a, b) => {
+  return a + b;
+};
 
 // const add = (a, b) => a + b;
-// console.log(add(1, 2));
+ console.log(add(1, 2));
 
 
 // const addOne = a => a + 1;
 // console.log(addOne(1));
 
 
-const addRandom = () => 1 + 2;
-console.log(addRandom());
+// const addRandom = () => 1 + 2;
+// console.log(addRandom());
 
 
